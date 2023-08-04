136ea7532a70a87fbbe13a62f05547e99423a0f9
20120234 <20120234@student.hcmus.edu.vn>
2023-08-04 19:09:48 +0700

    add rotate effect on geometry

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { Suspense, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import {
     Decal,
     OrbitControls,
@@ -8,24 +8,32 @@ import {
     useTexture
 } from '@react-three/drei';
 import Loader from '../Loader';
+import { easing } from 'maath';
 
 function BallDecal({ icon }) {
     const [decal] = useTexture([icon]);
+    const mesh = useRef();
+    
+    useFrame(({clock}) => {
+        mesh.current.geometry.rotateX(clock.getElapsedTime() % 5 * 0.01);
+    });
+    
     return (
         <Float
+
             speed={10}
             floatIntensity={2}
             rotationIntensity={1}
         >
             <ambientLight intensity={0.25} />
-            <directionalLight position={[0, 0, 0.05]} />
-            
+            <directionalLight position={[0, 0, 0.2]} />
             <mesh
+                ref={mesh}
                 castShadow
                 receiveShadow
                 scale={2.45}
             >
-                <icosahedronGeometry args={[1, 1]} />
+                <icosahedronGeometry args={[1, 2]}/>
                 <meshStandardMaterial
                     color='#fff8eb'
                     polygonOffset
@@ -33,8 +41,8 @@ function BallDecal({ icon }) {
                     flatShading
                 />
                 <Decal
-                    position={[0, 0, 1]}
-                    rotation={[6.25, 0, 6.25]}
+                    position={[0, 0, 0.8]}
+                    rotation={[-0.2, 0, 0]} // radian = degree * Math.PI / 180
                     scale={1}
                     map={decal}
                 />
@@ -46,7 +54,7 @@ function BallDecal({ icon }) {
 function Ball({ tech }) {
     return (
         <Canvas
-            frameloop='demand'
+            frameloop='demand' // set to 'always' to apply changes to each frame
             dpr={[1, 2]}
         >
             <Suspense fallback={<Loader />}>
