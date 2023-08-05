cbf9364cf38cc5abff365bb117bb0d09a1827658
20120234 <20120234@student.hcmus.edu.vn>
2023-08-06 02:22:51 +0700

    add background behind earth object

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import { slideIn, textVariant } from '../utils/motion';
 import Wrapper from './Wrapper';
 import Earth from './canvas/Earth';
+import Star from './canvas/Star';
 
 
 function Contact() {
@@ -26,78 +27,85 @@ function Contact() {
 
     return (
         <Wrapper idName="contact">
-            <div className='mt-4 flex flex-1 xl:flex-row flex-col-reverse gap-8 overflow-hidden'>
-                <motion.div
-                    variants={slideIn('left', 'tween', 2, 1)}
-                    className=' bg-black-200 p-6 rounded-xl xl:min-w-[480px]'
-                >
+            <div className='relative z-0'>
+                <div className='flex flex-1 xl:flex-row flex-col-reverse gap-8 overflow-hidden'>
                     <motion.div
-                        variants={textVariant(1.5)}
+                        variants={slideIn('left', 'tween', 2, 1)}
+                        className=' bg-black-200 p-6 rounded-xl xl:min-w-[480px]'
                     >
-                        <p className='sm:text-lg text-sm text-secondary uppercase tracking-wider'>Get in touch</p>
-                        <h2 className='text-white font-extrabold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'>Contact.</h2>
+                        <motion.div
+                            variants={textVariant(1.5)}
+                        >
+                            <p className='sm:text-lg text-sm text-secondary uppercase tracking-wider'>Get in touch</p>
+                            <h2 className='text-white font-extrabold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'>Contact.</h2>
+                        </motion.div>
+                        <form
+                            onSubmit={handleSubmit}
+                            className='mt-10 flex flex-col gap-8'
+                        >
+                            <div
+                                className='flex flex-col space-y-2'
+                            >
+                                <label className='text-white font-medium' htmlFor='name'>Your Name</label>
+                                <input
+                                    type='text'
+                                    name='name'
+                                    id='name'
+                                    value={form.name}
+                                    onChange={handleChange}
+                                    placeholder='Enter your name here..'
+                                    className='bg-tertiary p-4 text-white font-medium rounded-lg border-none placeholder:text-secondary'
+                                />
+                            </div>
+                            <div
+                                className='flex flex-col space-y-2'
+                            >
+                                <label className='text-white font-medium' htmlFor='email'>Your Email</label>
+                                <input
+                                    type='text'
+                                    name='email'
+                                    id='email'
+                                    value={form.email}
+                                    onChange={handleChange}
+                                    placeholder='Enter your email here..'
+                                    className='bg-tertiary p-4 text-white font-medium rounded-lg border-none placeholder:text-secondary'
+                                />
+                            </div>
+                            <div
+                                className='flex flex-col space-y-2'
+                            >
+                                <label className='text-white font-medium' htmlFor='message'>Your Message</label>
+                                <textarea
+                                    rows={8}
+                                    name='message'
+                                    id='message'
+                                    value={form.message}
+                                    onChange={handleChange}
+                                    placeholder='Leave what you wanna say here..'
+                                    className='bg-tertiary p-4 text-white font-medium rounded-lg border-none placeholder:text-secondary'
+                                />
+                            </div>
+                            <button
+                                type='submit'
+                                className='bg-tertiary w-fit text-white text-center font-bold rounded-xl py-2 px-4'
+                            >
+                                {loading ? 'Sending..' : 'Send'}
+                            </button>
+                        </form>
                     </motion.div>
-                    <form
-                        onSubmit={handleSubmit}
-                        className='mt-10 flex flex-col gap-8'
-                    >
-                        <div
-                            className='flex flex-col space-y-2'
-                        >
-                            <label className='text-white font-medium' htmlFor='name'>Your Name</label>
-                            <input
-                                type='text'
-                                name='name'
-                                id='name'
-                                value={form.name}
-                                onChange={handleChange}
-                                placeholder='Enter your name here..'
-                                className='bg-tertiary p-4 text-white font-medium rounded-lg border-none placeholder:text-secondary'
-                            />
-                        </div>
-                        <div
-                            className='flex flex-col space-y-2'
-                        >
-                            <label className='text-white font-medium' htmlFor='email'>Your Email</label>
-                            <input
-                                type='text'
-                                name='email'
-                                id='email'
-                                value={form.email}
-                                onChange={handleChange}
-                                placeholder='Enter your email here..'
-                                className='bg-tertiary p-4 text-white font-medium rounded-lg border-none placeholder:text-secondary'
-                            />
-                        </div>
-                        <div
-                            className='flex flex-col space-y-2'
-                        >
-                            <label className='text-white font-medium' htmlFor='message'>Your Message</label>
-                            <textarea
-                                rows={8}
-                                name='message'
-                                id='message'
-                                value={form.message}
-                                onChange={handleChange}
-                                placeholder='Leave what you wanna say here..'
-                                className='bg-tertiary p-4 text-white font-medium rounded-lg border-none placeholder:text-secondary'
-                            />
-                        </div>
-                        <button
-                            type='submit'
-                            className='bg-tertiary w-fit text-white text-center font-bold rounded-xl py-2 px-4'
-                        >
-                            {loading ? 'Sending..' : 'Send'}
-                        </button>
-                    </form>
-                </motion.div>
-                <motion.div 
-                    variants={slideIn('right','tween',2,1)}
-                    className='w-full xl:h-auto h-[460px] xl:-mt-3 -mt-32'>
-                    <Earth />
+                    <motion.div
+                        variants={slideIn('right', 'tween', 2, 1)}
+                        className='w-full xl:h-auto h-[460px] xl:-mt-3 -mt-32'>
+                        <Earth />
+                    </motion.div>
+                </div>
+                <motion.div
+                    variants={slideIn('down', 'spring', 0.1, 2)}
+                    className='absolute w-full h-full z-[-1] top-0 left-0'
+                >
+                    <Star />
                 </motion.div>
             </div>
-
         </Wrapper>
     )
 }
